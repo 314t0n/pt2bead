67861ea4d1268549684952ad3ffb078268816de3
hajnaldavid <hajnaldavid@caesar.elte.fake>
2014-06-09 22:53:29 +0200

    npex

--- src/gui/MainFrame.java
+++ src/gui/MainFrame.java
@@ -1,5 +1,9 @@
 package gui;
 
+import gui.dialogs.FormDialog;
+import gui.panels.AddCategoryPanel;
+import gui.panels.AddProductPanel;
+import gui.tablemodels.GenericTableModel;
 import java.awt.BorderLayout;
 import java.awt.HeadlessException;
 import java.awt.event.ActionEvent;
@@ -14,11 +18,16 @@ import javax.swing.JTable;
 import javax.swing.JToolBar;
 import static javax.swing.WindowConstants.DISPOSE_ON_CLOSE;
 import javax.swing.table.TableRowSorter;
+import logic.GenericDAO;
+import logic.Logger;
+import logic.entites.Category;
+import logic.entites.Product;
 
 public class MainFrame extends JFrame {
 
-    //private final GenericTableModel<Album, GenericDAO<Album>> albumTableModel;
-    //private final TableRowSorter<GenericTableModel<Album, GenericDAO<Album>>> albumSorter;
+    private GenericTableModel<Product, GenericDAO<Product>> productTableModel;
+    private TableRowSorter<GenericTableModel<Product, GenericDAO<Product>>> productSorter;
+
     public MainFrame() throws HeadlessException {
 
         setTitle("Generic ZH");
@@ -29,39 +38,51 @@ public class MainFrame extends JFrame {
         JToolBar jToolBar = new JToolBar();
 
         getContentPane().add(jToolBar, BorderLayout.NORTH);
-        JTable albumTable = new JTable();
         
         setMenu();
-        /*
-         albumTableModel = new GenericTableModel(new GenericDAO(Album.class), Album.getPropertyNames());
-         albumSorter = new TableRowSorter<>(albumTableModel);
 
-         albumTableModel.setColumnEditAble(2);
-
-         albumTable.setRowSorter(albumSorter);
-         albumTable.setModel(albumTableModel);
-
-         getContentPane().add(new JScrollPane(albumTable), BorderLayout.CENTER);
-         */
+        setProductTable();
     }
-    
+
+    private void setProductTable() {
+        try{
+        JTable productTable = new JTable();
+        
+        productTableModel = new GenericTableModel(new GenericDAO(Product.class), Product.getPropertyNames());
+        productSorter = new TableRowSorter<>(productTableModel);
+
+        productTableModel.setColumnEditAble(2);
+
+        productTable.setRowSorter(productSorter);
+        productTable.setModel(productTableModel);
+
+        getContentPane().add(new JScrollPane(productTable), BorderLayout.CENTER);
+        }catch(Exception ex){
+            ex.printStackTrace();
+        }
+
+    }
+
+    private final GenericDAO<Product> productController = new GenericDAO(Product.class);
+    private final GenericDAO<Category> categoryController = new GenericDAO(Category.class);
+
     /*
     
-    MENU
-    AND FUNCTIONS
+     MENU
+     AND ACTIONS 
     
-    
-    */
-    
-     private void setMenu() {
+     */
+    private void setMenu() {
 
         JMenuBar jMenuBar = new JMenuBar();
         JMenu mainMenu = new JMenu("File");
 
-        JMenuItem newGame = new JMenuItem(newProductAction);            
+        JMenuItem addProduct = new JMenuItem(addProductAction);
+        JMenuItem addCategory = new JMenuItem(addCategoryAction);
         JMenuItem close = new JMenuItem(closeAction);
 
-        mainMenu.add(newGame);     
+        mainMenu.add(addProduct);
+        mainMenu.add(addCategory);
         mainMenu.addSeparator();
         mainMenu.add(close);
         jMenuBar.add(mainMenu);
@@ -69,17 +90,55 @@ public class MainFrame extends JFrame {
         setJMenuBar(jMenuBar);
 
     }
-     
-     /**
-     * Új játék kezdése
-     */
-    private Action newProductAction = new AbstractAction("Új termék") {
+
+    private Action addProductAction = new AbstractAction("Új termék") {
         @Override
         public void actionPerformed(ActionEvent e) {
-            
+
+            Product product = new Product();
+
+            AddProductPanel addProductPanel = new AddProductPanel(product, categoryController.readAll());
+            FormDialog formDialog = new FormDialog(MainFrame.this, enabled, addProductPanel);
+
+            if (formDialog.isSave()) {
+
+                addProductPanel.setAttributes();
+
+                Logger.log("Adatok mentése", "DEBUG");
+
+                productController.create(product);
+
+            }
+
         }
     };
-   
+
+    private Action addCategoryAction = new AbstractAction("Új kategória") {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+            Category category = new Category();
+
+            AddCategoryPanel addCategoryPanel = new AddCategoryPanel(category);
+            FormDialog formDialog = new FormDialog(MainFrame.this, enabled, addCategoryPanel);
+
+            if (formDialog.isSave()) {
+
+                Logger.log("Adatok mentése", "DEBUG");
+
+                addCategoryPanel.setAttributes();
+
+                if (category != null) {
+
+                    categoryController.create(category);
+
+                }
+
+            }
+
+        }
+    };
+
     private Action closeAction = new AbstractAction("Kilépés") {
         @Override
         public void actionPerformed(ActionEvent e) {
